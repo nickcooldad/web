de1713570b0e527ba7970267317eaced519b8ca3
nickcooldad <nickcooldad68@gmail.com>
2024-09-10 22:30:38 +0300

    hmwrk

--- newfigma/src/App.js
+++ newfigma/src/App.js
@@ -1,12 +1,42 @@
 import {ProgressBar} from './components/progressBar.jsx'
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const [data, setData] = useState({arrayInput:[], value:0})
+  const width = {width : 900 / data.arrayInput.length}
+  const hundleInputArrayChange = (event) => {
+    const array = event.target.value.split(',').map(item => item.trim())
+    setData(prev => ({
+      ...prev,
+      arrayInput: array
+    }))
+  }
+
+  const hundleInputValueChange = (event) => {
+  setData(prev => ({
+  ...prev,
+  value: event.target.value}))
+  }
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    if (elementRef.current) {
+      const rect = elementRef.current.getBoundingClientRect();
+      const width1 = rect.width;
+      console.log('=_=_',width1);
+    }
+  }, [data]);
+
   return (
     <div className="App">
-     <ProgressBar />
-    </div>
+     <ProgressBar 
+     thresholds={data.arrayInput} 
+     value={data.value} 
+     hundleInputArrayChange={hundleInputArrayChange} 
+     hundleInputValueChange={hundleInputValueChange}
+     width={width}/>
+    </div>  
   );
 }
-
 export default App;
--- newfigma/src/components/progressBar.jsx
+++ newfigma/src/components/progressBar.jsx
@@ -1,55 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+//import st from 'classnames'
 import './progressBar.css'
 import { Cup } from './images/cup.jsx';
 import { Star } from './images/star.jsx';
 
-export  function ProgressBar() {
- 
-  const [data, setData] = useState({arrayInput : [], value : 0})
-  const width = {width : 900 / data.arrayInput.length}
-  const handleInputArrayChange = (event) => {
-    const array = event.target.value.split(',').map(item => item.trim())
-    setData(prev => ({
-      ...prev,
-      arrayInput: array
-    }))
-  }
+export  function ProgressBar({thresholds, value, hundleInputArrayChange, hundleInputValueChange, width}) {
 
-  const handleInputValueChange = (event) => {
-  setData(prev => ({
-  ...prev,
-  value: event.target.value}))
-  }
+
+
+
 
   return (
     <>
-     <input className='inputArray' placeholder='Введите ряд чисел через запятую= 10,20,30,40' value={data.arrayInput.join(',')} onChange={handleInputArrayChange}></input>
-     <input className='inputValue' placeholder='Введите значение' value={data.value} onChange={handleInputValueChange}></input>
+     <input className='inputArray' placeholder='Введите ряд чисел через запятую= 10,20,30,40' value={thresholds.join(',')} onChange={hundleInputArrayChange}></input>
+     <input className='inputValue' placeholder='Введите значение' value={value} onChange={hundleInputValueChange}></input>
     <div className='parentBar' >
-      {data.arrayInput.map((bar, index) => {
-        const lastItemIndex = data.arrayInput.length - 1;
-        const isFilled = data.value >= bar;
-        const prevThreshold = index > 0 ? data.arrayInput[index - 1] : 0;
-        const fillPercentage = isFilled ? 100 : (data.value - prevThreshold) / (bar - prevThreshold) * 100;
+      {thresholds.map((bar, index) => {
+        const lastItemIndex = thresholds.at(-1);
+        const isFilled = value >= bar;
+        const prevThreshold = index > 0 ? thresholds[index - 1] : 0;
+        const fillPercentage = isFilled ? 100 : (value - prevThreshold) / (bar - prevThreshold) * 100;
         return <div className='progressBar' style={width}>
           
-          <div className='topBlock' style={{width: 900/data.arrayInput.length + 10}}>
+          <div className='topBlock' style={{width: 900/thresholds.length + 10}}>
             {index !== lastItemIndex ? <Star color={isFilled ? "blue" : 'none'}/> : <Cup color={isFilled ? "blue" : '#e0e0e0'} style={{marginRight: index === lastItemIndex ? '10px' : ''}}/>}
           </div>
+
           <div className='centerBlock' style={
-            { borderRight: index !== lastItemIndex ? '2px solid #c4bebe' : '',
-              borderTopLeftRadius: index === 0 ? '25px' : '',
-              borderBottomLeftRadius: index === 0 ? '25px' : '',
-              borderTopRightRadius: index === lastItemIndex ? '25px' : '',
-              borderBottomRightRadius: index === lastItemIndex ? '25px' : '',
+            { 
               background: `linear-gradient(to right, rgb(70, 27, 242) ${fillPercentage}%, #e0e0e0 ${fillPercentage}%)`,
-              position: 'relative',
             }}> 
             </div>
-          <div className='bottomBlock' style={{width: 900/data.arrayInput.length + 10}}>
-            <div className='bottomBlockNull'>{index === 0 || (index === 0 && index === data.arrayInput.length - 1) ? '0' : ''}</div>
-            <div className='bottomBlockNumber' style={{marginRight: index === lastItemIndex ? '10px' : ''}}>{index === 0 && data.value <= data.arrayInput[0] ?  `${data.value}/${data.arrayInput[0]}` : index === lastItemIndex && data.value > data.arrayInput[0] ? `${data.value > data.arrayInput.at(-1) ? data.arrayInput.at(-1) : data.value}/${data.arrayInput.at(-1)}` : bar}</div>
+ 
+          <div className='bottomBlock' style={{width: 900/thresholds.length + 10}}>
+            <div className='bottomBlockNull'>{index === 0 || (index === 0 && index === thresholds.length - 1) ? '0' : ''}</div>
+            <div className='bottomBlockNumber' style={{marginRight: index === lastItemIndex ? '10px' : ''}}>
+              {index === 0 && value <= thresholds[0] 
+              ?`${value}/${thresholds[0]}` : index === lastItemIndex && value > thresholds[0] 
+              ? `${value > thresholds.at(-1) ? thresholds.at(-1) : value}/${thresholds.at(-1)}` : bar}
+              </div>
           </div>
+
         </div>
       })}
     </div>
