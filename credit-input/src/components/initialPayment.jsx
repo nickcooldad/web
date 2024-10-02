f409a60dc965fd1fe27df89a6b132f2379c8fedf
nickcooldad <nickcooldad68@gmail.com>
2024-10-02 22:53:33 +0300

    hhmwrk

---
+++ credit-input/src/components/initialPayment.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export function InitialPayment({name, limit, type}) {
+  return (
+    <>
+    <h1>{name}</h1>
+    <div>
+        <input/>
+        <input/>
+    </div>
+    <div>
+        
+    </div>
+    </>
+  )
+}
