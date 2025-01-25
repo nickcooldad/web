626d58b3d066b4515913e47c75de31d2f87ed6ef
nickcooldad <nickcooldad68@gmail.com>
2025-01-25 13:53:08 +0300

    hmrk

--- ts/tabble-users/src/components/TableUsers.tsx
+++ ts/tabble-users/src/components/TableUsers.tsx
@@ -9,30 +9,34 @@ interface TableUsersProps <T>{
 interface SortProperty <T>{
   stateSort : boolean,
   propertySort: TableResponse<T> | null,
-  data : T[]
+ // data : T[]
  }
  
 export function TableUsers<T>({title, dataTable}: TableUsersProps<T>) {
 
   const [dataSort, setDataSort] = useState<SortProperty<T>>({
     propertySort: null,
-    stateSort : false,
-    data: dataTable
+    stateSort : false
+   // data: dataTable
   })
-  
+console.log(dataSort)
+let sortedData = [...dataTable]
+
+const {propertySort} = dataSort
+if(propertySort !== null){
+  sortedData.sort(propertySort.tableSort)
+} 
+if(dataSort.stateSort){
+  sortedData.reverse()
+}
+
 function handleClickSortedProperty(property: TableResponse<T>) {
   setDataSort(prev => { 
-    const sorted = prev.data.toSorted((a,b) => property.tableSort ? property.tableSort(a,b) : 0)
-    const result = dataSort.stateSort ? sorted.reverse() : sorted
-
-        if(prev.propertySort === property) return  {...prev, stateSort: !prev.stateSort, data: result}
-        return  {...prev, stateSort : true, propertySort: property, data: result}
+        if(prev.propertySort === property) return  {...prev, stateSort: !prev.stateSort}
+        return  {...prev, stateSort : true, propertySort: property }
       })
     }
 
-
-
-console.log(dataSort)
   return (
     <table className={s.styleTable}>
       <thead>
@@ -40,7 +44,10 @@ console.log(dataSort)
           {title.map((property, index) => (
             <th key={index} >
               {property.titleName}
-              {property.tableSort && <button className={cn(s.styleButton, {[s.active] : property === dataSort.propertySort && dataSort.stateSort})} onClick={() => {
+              {property.tableSort && <button className={cn(s.styleButton, {
+                [s.active] : property === dataSort.propertySort && dataSort.stateSort,
+                [s.passiv] : property === dataSort.propertySort && !dataSort.stateSort
+              })} onClick={() => {
                 handleClickSortedProperty(property)
                 }}></button>}
             </th>
@@ -48,7 +55,7 @@ console.log(dataSort)
         </tr>
       </thead>
       <tbody>
-        {dataSort.data.map((user, rowIndex) => (
+        {sortedData.map((user, rowIndex) => (
           <tr key={rowIndex}>
             {title.map((property, colIndex) => (
               <td key={colIndex}>{property.tableBody(user)}</td>
