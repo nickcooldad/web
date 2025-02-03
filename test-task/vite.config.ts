91c2744447919482105faf970752f291036fd3c8
nickcooldad <nickcooldad68@google.com>
2025-02-03 16:26:46 +0300

    hmwrk

--- test-task/vite.config.ts
+++ test-task/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web/test-task/',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
